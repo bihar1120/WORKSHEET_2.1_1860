6f699ac3861c5d4795d443dfe3159d6a2b856f2e
Shreya Shambhavi <ztgq5019@leeds.ac.uk>
2025-03-08 18:23:40 +0000

    Update Xor.asm

--- Xor.asm
+++ Xor.asm
@@ -1,19 +1,22 @@
 // Step 1: Calculate A AND (NOT B)
 @B           // Load address of B
-M=!M         // Invert B (NOT B)
+D=M          // D = B
+M=!D         // Store NOT B in B (Invert B)
 @A           // Load address of A
 D=M          // D = A
 D=D&M        // D = A AND (NOT B) (Result of first part of XOR)
 
 // Step 2: Calculate (NOT A) AND B
 @A           // Load address of A
-M=!M         // Invert A (NOT A)
-@B // Load address of B
-D=M
+D=M          // D = A
+M=!D         // Store NOT A in A (Invert A)
+@B           // Load address of B
 D=D&M        // D = (NOT A) AND B (Result of second part of XOR)
+
 // Step 3: Combine the two results using OR to get the final XOR result
 @RESULT      // Load address of RESULT
 M=D|M        // Store (A AND (NOT B)) OR ((NOT A) AND B) in RESULT
+
 
 
 
